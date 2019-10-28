23841ccbc884a3191d6f9e22c49fcf507b5a1bac
Alejandro <alejandrogibran@hotmail.com>
2019-10-27 19:42:58 -0600

    Feature Extraction: Almacenamiento con Pickles
    Ahora el proceso de Feature extraction se guarda en archivos con la librearía Pickles, para no guardar esos datos en memoria cada vez que se quiere probar el modelo. Esta técnica servira más adelante para guardar el modelo entrenado en un archivo binario

--- dashboard/backend/preprocesamiento/FeatureExtraction.py
+++ dashboard/backend/preprocesamiento/FeatureExtraction.py
@@ -1,11 +1,13 @@
 # Importacion de librerias
+from sklearn.utils import resample
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import nltk
 import csv
-from sklearn.utils import resample
-import ast
-import json
+import pickle
+import sys
+# import json
 
 class FeatureExtraction:
     word_features = []
@@ -16,12 +18,14 @@ class FeatureExtraction:
     columna_sentimiento = ""
 
     def __init__(self, archivo, columna_texto, columna_sentimiento):
+        print("=== INICIANDO CLASE: FEATURE EXTRACTION ===")
         self.archivo = archivo
         self.columna_texto = columna_texto
         self.columna_sentimiento = columna_sentimiento
         self.crearConjuntos()
 
     def crearConjuntos(self):
+        print("=== CREACION CONJUNTOS DE ENTRENAMIENTO Y PRUEBAS ===")
         # Permite desplegar el texto completo en Jupyter y en Terminal
         pd.set_option('display.max_colwidth', -1)
 
@@ -33,10 +37,14 @@ class FeatureExtraction:
         # Conjunto de Entrenamiento y Pruebas
         porcentaje = int(len(data_lemmatized) * .20)
         train_set, self.test_set = data[porcentaje:], data[:porcentaje]
+        print("train_set: ", train_set.shape)
+        print("test_set: ", self.test_set.shape)
         self.train_set_upsampled = self.resampling(train_set, self.columna_sentimiento)
 
+        print("Guardando el remuestreo de train_set en CSV...")
         # Guardar conjunto de entrenamiento y pruebas en CSV
         train_csv = pd.DataFrame(self.train_set_upsampled).to_csv (r'train_set_upsampled.csv', index = None, header=True)
+        print("Guardando test_set en CSV...")
         test_csv = pd.DataFrame(self.test_set).to_csv (r'test_set.csv', index = None, header=True)
 
     # Crear un Bag of Words
@@ -54,9 +62,9 @@ class FeatureExtraction:
         return list(all_words_freq)[:2000]
 
 
-
     # Definir el feature extractor
     def document_features(self, document):
+        sys.stdout.flush()
         document_words = set(document)
         features = {}
         for word in self.word_features:
@@ -67,8 +75,11 @@ class FeatureExtraction:
 
     # Resampling
     def resampling(self, train_set, columna_sentimiento):
+        print("\n=== RESAMPLING A TRAIN_SET ===")
         # Obtener los sentimientos en el dataframe y la cantidad que existen
         contador_sentimientos = train_set[columna_sentimiento].value_counts()
+        print("Sentimientos antes del Resampling: ")
+        print(contador_sentimientos)
 
         # Obtener la cantidad más grande
         mayor = contador_sentimientos.max()
@@ -88,7 +99,7 @@ class FeatureExtraction:
         df_majority = train_set[train_set.sentiment == sentimiento_mayor]
         df_minority_1 = train_set[train_set.sentiment == lista_contador_sentimientos[0]]
         df_minority_2 = train_set[train_set.sentiment == lista_contador_sentimientos[1]]
-        
+        print("Resampling en progreso...")
         # Upsample minority class
         df_upsampled_1 = resample(df_minority_1, 
                                         replace=True,     # sample with replacement
@@ -102,43 +113,75 @@ class FeatureExtraction:
         
         # Combine majority class with upsampled minority class
         train_upsampled = pd.concat([df_majority, df_upsampled_1, df_upsampled_2])
+        print("Resultados del Resampling:")
+        print(train_upsampled[columna_sentimiento].value_counts())
         return train_upsampled
 
 
     # Preparar los features del conjunto de entrenamiento y pruebas
     def featuresPreparation(self, train_upsampled_file, test_set_file):
+        print("\n=== FEATURE PREPARATION ===")
+        print("Lectura de CSV...")
         # Leer CSV con los archivos del conjunto de entrenamiento y prueba sin features
         train_set = pd.read_csv(train_upsampled_file)
         test_set = pd.read_csv(test_set_file)
         # Convertir el conjunto de entrenamiento y pruebas a una listas
         train = train_set.values.tolist()
         test = test_set.values.tolist()
-
-
-        # Crear los conjuntos de entrenamiento y prueba con las carácterísticas (pivoteo de Bag of Words)
-        train_feature_sets = [(self.document_features(str(d)), c) for (d,c) in train]
-        test_feature_sets = [(self.document_features(str(d)), c) for (d,c) in test]
         
-        # Guardar como JSON
-        #with open('train_feature_column.json', 'w') as jsonFile:
-        #    for lists in train_feature_sets:
-        #        json.dump(lists[0] , jsonFile)
+        # Crear los conjuntos de entrenamiento y prueba con las carácterísticas (pivoteo de Bag of Words)
+        # Dividir el train en partes
+        filas = int(train_set.shape[0])
+        print(filas)
+        if filas > 2500:
+            self.divisor = int(filas / 2500)
+            intervalos = int(filas / divisor)
+        else:
+            intervalos = 1
+        print(divisor)
+        print(intervalos)
+        print("Iniciando el Feature Extraction (pivoteo): ")
+        inicio = 0
+        limite = 0
+        for i in range(self.divisor):
+            # Definir limite derecho
+            limite += intervalos
+            # Decisiones de intervalos
+            print(f"Training set: {i}")
+            if i == 0:                  # Si es la primera iteracion
+                train_feature_sets = [(self.document_features(str(d)), c) for (d,c) in tqdm(train[:limite])]
+            elif (i+1) == divisor:      # Si es la última iteracion
+                train_feature_sets = [(self.document_features(str(d)), c) for (d,c) in tqdm(train[limite:])]
+            else:                       # Si es cualquier otra iteracion
+                train_feature_sets = [(self.document_features(str(d)), c) for (d,c) in tqdm(train[inicio:limite])]
+            # Guardar Pickle
+            self.trainingPickle(f"train_feature_sets_{i}", train_feature_sets)
+            # Definir limite izquierdo
+            inicio = limite
+                
+        print("Test set:")
+        test_feature_sets = [(self.document_features(str(d)), c) for (d,c) in tqdm(test)]
+        print("¡Feature Extraction completado con exito!")
+        self.trainingPickle("Test_feature_sets", test_feature_sets)
 
-        train_sentiment_csv = pd.DataFrame(train_feature_sets).to_csv (r'train_feature_sets.csv', index = None, header=True)
-        test_csv = pd.DataFrame(test_feature_sets).to_csv (r'test_feature_sets.csv', index = None, header=True)
-
-
-    def modeling(self, train_csv, test_csv):
-        # Read CSV with train and test features
-        feature_train = pd.read_csv(train_csv)
-        feature_test = pd.read_csv(test_csv)
-
-        # convertir primera columna a diccionario
-        feature_train['0'] = feature_train['0'].apply(ast.literal_eval)
-        feature_test['0']  = feature_test['0'].apply(ast.literal_eval)
-
-        feature_train_list = feature_train.values.tolist()
-        feature_test_list = feature_test.values.tolist()
+    def modeling(self, divisor):
+        print("\n=== MODELING ===")        
+        # Read Pickles with train and test features
+        print("Lectura de Pickles")
+        print("Training set...")
+        feature_train_list = []
+        for i in range(divisor):
+            print(f"Abriendo Pickle_{i}...")
+            with open(f'train_feature_sets_{i}', 'rb') as infile:
+                training_list = pickle.load(infile)
+            print(f"Pickle_{i} abierto con exito")
+            feature_train_list += training_list
+        
+        print("Test set:...")
+        print("Abriendo Pickle...")
+        with open('test_feature_sets', 'rb') as infile:
+            feature_test_list = pickle.load(infile)
+        print(f"Pickle_{i} abierto con exito")
 
         print(feature_test_list[:2][:])
         # Train de model (Naive Bayes)
@@ -150,7 +193,14 @@ class FeatureExtraction:
         # Print 100 most significative words in analysis
         print(classifier.show_most_informative_features(100))
 
-
+    def trainingPickle(self, name, datos):
+            # Guardando como pickle
+            print(f"Guardando pickle: {name}...")
+            # Training Set
+            with open(name, 'wb') as pickleFile:
+                pickle.dump(datos, pickleFile)
+            print(f"¡Pickle {name} guardado con éxito!")
+            
 #fe = FeatureExtraction("data_lemmatized.csv", "data_lemmatized", "sentiment")
 #fe.featuresPreparation("train_set_upsampled.csv", 'test_set.csv')
-#fe.modeling("train_feature_sets.csv", "test_feature_sets.csv")
+#fe.modeling(8)
