4c4f28def6665996047d04b2c209f9aaf2388874
Alejandro <alejandrogibran@hotmail.com>
2019-11-06 17:49:15 -0600

    Modelos Logistic Regression y SVM. Pickles de Feature Extraction
    Se añadieron los modeulos para entrenar los modelos de LogisticRegression y Support Vector Machines (SVM) en el archivo FeatureExtraction2.py. Ahora tambien es posible guardar el Upsampling SMOTE en Pickles para reducir los tiempos de ejecucion

--- dashboard/backend/preprocesamiento/AnalisisSentimientos.py
+++ dashboard/backend/preprocesamiento/AnalisisSentimientos.py
@@ -8,12 +8,16 @@ import sys
 def importarLibrarias():
     subprocess.call([sys.executable, "-m", "pip", "install","-r", "requirements.txt"])
 
+
+def entrenar(dataset_entrenamiento, columna_tweets, columna_sentimientos):
+    #prep = preprocesamiento(dataset_entrenamiento, columna_tweets, columna_sentimientos)
+    #prep.preparacion()
+    print("=== ENTRENAMIENTO INICIADO ===")
+    fe = feature_extraction("data_lemmatized.csv", 'data_lemmatized', "sentiment")
+    fe.extraction('tfidf')
+    print("=== ENTRENAMIENTO TERMINADO EXITOSAMENTE ===")
+
+
 def predecir(dataset, columna_tweets):
     pred = prediction("MultinomialNB", 'CountVectorizer', dataset, columna_tweets)
     pred.predecir()
-
-def entrenar(dataset_entrenamiento, columna_tweets, columna_sentimientos):
-    prep = preprocesamiento(dataset_entrenamiento, columna_tweets, columna_sentimientos)
-    prep.preparacion()
-    fe = feature_extraction("data_lemmatized.csv", 'data_lemmatized', "sentiment")
-    fe.extraction()
--- dashboard/backend/preprocesamiento/FeatureExtraction2.py
+++ dashboard/backend/preprocesamiento/FeatureExtraction2.py
@@ -3,6 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 from sklearn.metrics import confusion_matrix
 from LongFunctionProgress import provide_progress_bar
 from LongFunctionProgress import progress_wrapped
@@ -21,8 +23,9 @@ class FeatureExtraction:
         self.dataset = dataset
         self.columna_tweets = columna_tweets
         self.columna_sentimiento = columna_sentimiento
+        self.ruta_actual = PurePath(Path.cwd())
 
-    def extraction(self):
+    def extraction(self, extractor):
         print("=== FEATURE EXTRACTION INICIADO ===")
         # Cargando rutas y lectura dataset
         x, y = self.path('DB')
@@ -31,50 +34,45 @@ class FeatureExtraction:
         X_train, X_test, y_train, y_test = self.split(x, y)
 
         # Feature Extraction
-        print("Feature Extraction: Bag of Words...")
-        X_features_train, X_features_test, vectorizer = self.bagOfWords(
-            X_train, X_test)
+        if extractor == 'bagofwords':
+            print("Feature Extraction: Bag of Words...")
+            X_features_train, X_features_test, vectorizer = self.bagOfWords(
+                X_train, X_test)
+        elif extractor == 'tfidf':
+            X_features_train, X_features_test, vectorizer = self.TfidfVectorizer(
+                X_train, X_test)
+        else:
+            print('Extractor no seleccionado. Terminando')
+            return
 
         # Upsamping SMOTE
         print("Upsamping SMOTE...")
-        X_resampled, y_resampled = self.overSamplingSMOTE(
-            X_features_train, y_train, vectorizer)
-
-        # Modeling
-        print("Modelado: MultinomialNB...")
-        clf = self.MultiNaiveBayes(X_resampled, X_features_test, y_resampled, y_test)
-
-        # Guardando como pickle
-        print(f"Guardando pickle: clf...")
-        # Training Set
-        # Ruta actual
-        ruta_actual = PurePath(Path.cwd())
-        # Direccion Pickle Clasificador
-        _archivo = ruta_actual / 'Classifiers' / 'MultinomialNB'
-        with open(_archivo, 'wb') as pickleFile:
-            pickle.dump(clf, pickleFile)
-        print(f"¡Pickle guardado con éxito!")
+        X_resampled_path = Path(self.ruta_actual / "DB/X_resampled")
+        y_resampled_path = Path(self.ruta_actual / "DB/y_resampled")
+        if(X_resampled_path.exists() and y_resampled_path.exists()):
+             X_resampled = self.loadPickle("DB/X_resampled")
+             y_resampled = self.loadPickle("DB/y_resampled")
+        else:
+            X_resampled, y_resampled = self.overSamplingSMOTE(
+                X_features_train, y_train, vectorizer)
         print("=== FEATURE EXTRACTION TERMINADO ===")
 
-    def path(self, carpeta):
-        # Ruta actual
-        ruta_actual = PurePath(Path.cwd())
-        # Direccion Pickle Clasificador
-        _archivo = ruta_actual / carpeta / self.dataset
-        # Leer Dataset
-        print("Leyendo Dataset...")
-        pd.set_option('display.max_colwidth', -1)
-        tweets = pd.read_csv(_archivo)
-        x = tweets[self.columna_tweets]
-        y = tweets[self.columna_sentimiento]
-        return x, y
+        # Modeling
+        print("=== MODELADO INICIADO ===")
+        print("Modelado: MultinomialNB...")
+        NB_clf = self.MultiNaiveBayes(X_resampled, X_features_test, y_resampled, y_test)
+        
+        print("Modelado: LogisticRegression...")
+        LR_clf = self.LogisticRegression(X_resampled, X_features_test, y_resampled, y_test)
 
-    def split(self, x, y):
-        # Conjunto de entrenamiento y prueba
-        print("Creando conjunto de entrenamiento y prueba...")
-        X_train, X_test, y_train, y_test = train_test_split(
-            x, y, test_size=0.33, random_state=53)
-        return X_train, X_test, y_train, y_test
+        # print("Modelado: SVM...")
+        # SVM_clf = self.SVM(X_resampled, X_features_test, y_resampled, y_test)
+        
+        print("=== MODELADO TERMINADO ===")
+        self.crearPickle(NB_clf, 'Classifiers/MultinomialNB')
+        self.crearPickle(LR_clf, 'Classifiers/LogisticRegression')
+        # self.crearPickle(SVM_clf, 'Classifiers/SVM')
+
 
 
     def bagOfWords(self,X_train, X_test):
@@ -89,16 +87,7 @@ class FeatureExtraction:
         print(count_df.head())
 
         # Guardando como pickle
-        print(f"Guardando pickle: vectorizer...")
-        # Training Set
-        # Ruta actual
-        ruta_actual = PurePath(Path.cwd())
-        # Direccion Pickle Vectorizador
-        _archivo = ruta_actual / 'Vectorizers' / 'CountVectorizer'
-        with open(_archivo, 'wb') as pickleFile:
-            pickle.dump(count_vectorizer, pickleFile)
-        print(f"¡Pickle guardado con éxito!")
-
+        self.crearPickle(count_vectorizer, 'Vectorizers/CountVectorizer')
         return count_train, count_test, count_vectorizer
 
     def TfidfVectorizer(self, X_train, X_test):
@@ -115,6 +104,9 @@ class FeatureExtraction:
         tfidf_df = pd.DataFrame(
             tfidf_train.A, columns=tfidf_vectorizer.get_feature_names())
         print(tfidf_df.head())
+
+        # Guardando como pickle
+        self.crearPickle(tfidf_vectorizer, 'Vectorizers/TfidfVectorizer')
         return tfidf_train, tfidf_test, tfidf_vectorizer
 
     def MultiNaiveBayes(self, X_train, X_test, y_train, y_test):
@@ -129,6 +121,33 @@ class FeatureExtraction:
         print(score)
         print(confusion_matrix(y_test, pred))
         return nb_classifier
+    
+    def LogisticRegression(self, X_train, X_test, y_train, y_test):
+        # Instanciar modelo LogisticRegression
+        lr_classifier = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
+        # Entrenar el modelo
+        lr_classifier.fit(X_train, y_train)
+        # Predecir con el modelo
+        pred = lr_classifier.predict(X_test)
+        # Evaluar el modelo
+        score = classification_report(y_test, pred)
+        print(score)
+        print(confusion_matrix(y_test, pred))
+        return lr_classifier
+
+    @progress_wrapped(estimated_time=1200)
+    def SVM(self, X_train, X_test, y_train, y_test):
+        # Instanciar modelo SVC
+        svm_classifier = svm.SVC(gamma='scale')
+        # Entrenar el modelo
+        svm_classifier.fit(X_train, y_train)
+        # Predecir con el modelo
+        pred = svm_classifier.predict(X_test)
+        # Evaluar el modelo
+        score = classification_report(y_test, pred)
+        print(score)
+        print(confusion_matrix(y_test, pred))
+        return svm_classifier
 
     @progress_wrapped(estimated_time=100)
     def overSamplingSMOTE(self, X_train, y_train, vectorizer):
@@ -140,7 +159,45 @@ class FeatureExtraction:
         # Create the resampled feature set
         X_resampled, y_resampled = method.fit_sample(X_df.to_numpy(), y_train.to_numpy())
         print(pd.value_counts(pd.Series(y_resampled)))
+        # Guardar en Pickles
+        self.crearPickle(X_resampled, "DB/X_resampled")
+        self.crearPickle(y_resampled, "DB/y_resampled")
         return X_resampled, y_resampled
+        
+    def path(self, carpeta):
+        # Direccion Pickle Clasificador
+        _archivo = self.ruta_actual / carpeta / self.dataset
+        # Leer Dataset
+        print("Leyendo Dataset...")
+        pd.set_option('display.max_colwidth', -1)
+        tweets = pd.read_csv(_archivo)
+        x = tweets[self.columna_tweets]
+        y = tweets[self.columna_sentimiento]
+        return x, y
 
+
+    def split(self, x, y):
+        # Conjunto de entrenamiento y prueba
+        print("Creando conjunto de entrenamiento y prueba...")
+        X_train, X_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.33, random_state=53)
+        return X_train, X_test, y_train, y_test
+
+    def crearPickle(self, obj , archivo):
+        # Guardando como pickle
+        print(f"Pickle: {archivo}...")
+        # Direccion Pickle Clasificador
+        _archivo = self.ruta_actual / archivo
+        with open(_archivo, 'wb') as pickleFile:
+            pickle.dump(obj, pickleFile)
+        print(f"Pickle: {archivo} guardado con éxito!")
+
+    def loadPickle(self, archivo_pickle):
+        _archivo = self.ruta_actual / archivo_pickle
+        print(f"Abriendo Pickle {archivo_pickle}...")
+        with open(_archivo, 'rb') as infile:
+            obj = pickle.load(infile)
+        print(f"Pickle {archivo_pickle} abierto con exito")
+        return obj
 
 # FeatureExtraction2("data_lemmatized.csv", 'data_lemmatized', "sentiment")
